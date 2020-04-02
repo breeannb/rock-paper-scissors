6ed66288776f6977b7df1fbcbb6213941aa9dbf9
Daniel Bennington <dbennington125@gmail.com>
2020-04-02 07:20:36 -0700

    get travis to work2

--- getRandomThrow.js
+++ getRandomThrow.js
@@ -2,6 +2,6 @@ export function generateComputerThrow(){
     let number = Math.floor(Math.random() * 3); //had to google this
     //need to change the number to a word value
     let wordOption = ['rock', 'paper', 'scissors'];//had to google
-    let compChoice = wordOption[number];//had hunter explain this to me...I'm very lost.
+    let compChoice = wordOption[number];//had Hunter explain this to me...I'm very lost.
     return compChoice;
 }
